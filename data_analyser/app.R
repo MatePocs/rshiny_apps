eb849a9be97c9ced98a75fa15211bb1bbafe89dc
Mate Pocs <pocsmate1987@gmail.com>
2021-04-11 15:06:09 +0100

    changed approach

--- data_analyser/app.R
+++ data_analyser/app.R
@@ -3,12 +3,6 @@ library(shinythemes)
 library(data.table)
 library(ggplot2)
 
-data_input <- data.table(
-  "NA" = c(NA)
-)
-
-plot_1 <- ggplot()
-
 about_page <- tabPanel(
   title = "About",
   titlePanel("About"),
@@ -21,26 +15,18 @@ main_page <- tabPanel(
   sidebarLayout(
     sidebarPanel(
       title = "Inputs",
-      fileInput(inputId = "csv_input", label = "Select CSV File to Import", accept = ".csv"), 
-      # selectInput(inputId = "column_1", label = "Variable 1", choices = c("Placeholder 1")),
-      # selectInput(inputId = "column_2", label = "Variable 2", choices = c("Placeholder 2")),
-      uiOutput("dyn_ui_select_1"),
-      uiOutput("dyn_ui_var_type_1"),
-      uiOutput("dyn_ui_select_2"),
-      uiOutput("dyn_ui_var_type_2"),
+      fileInput("csv_input", "Select CSV File to Import", accept = ".csv"), 
+      selectInput("num_var_1", "Numerical Variable 1", choices = c("Not Selected")),
+      selectInput("num_var_2", "Numerical Variable 2", choices = c("Not Selected")),
+      selectInput("fact_var", "Factor Variable", choices = c("Not Selected")),
       br(),
-      actionButton(inputId = "run_button", label = "Run Analysis", icon = icon("play"))
+      actionButton("run_button", "Run Analysis", icon = icon("play"))
     ),
     mainPanel(
       tabsetPanel(
         tabPanel(
           title = "Plots", 
           plotOutput("plot_1"), 
-          wellPanel(
-            checkboxInput(inputId = "log_x", label = "Log scale on axis X"),
-            checkboxInput(inputId = "log_y", label = "Log scale on axis Y")  
-          )
-          
         ),
         tabPanel(
           title = "Statistics", 
@@ -50,7 +36,6 @@ main_page <- tabPanel(
     )
   )
 )
-
 
 
 ui <- navbarPage(
@@ -65,49 +50,14 @@ server <- function(input, output){
   data_input <- reactive({
     req(input$csv_input)
     fread(input$csv_input$datapath)
-    })
-  
-  output$dyn_ui_select_1 <- renderUI({
-    selectInput(inputId = "var_1", label = "Variable 1", choices = colnames(data_input()))
   })
   
-  output$dyn_ui_var_type_1 <- renderUI({
-    req(data_input())
-    selectInput(inputId = "var_1_type", label = "Variable 1 Type", choices = c("Factor","Numerical"))
+  observeEvent(data_input(),{
+    choices <- c("Not Selected",names(data_input()))
+    updateSelectInput(inputId = "num_var_1", choices = choices)
+    updateSelectInput(inputId = "num_var_2", choices = choices)
+    updateSelectInput(inputId = "fact_var", choices = choices)
   })
-  
-  output$dyn_ui_select_2 <- renderUI({
-    selectInput(inputId = "var_2", label = "Variable 2", choices = colnames(data_input()))
-  })
-  
-  output$dyn_ui_var_type_2 <- renderUI({
-    req(data_input())
-    selectInput(inputId = "var_2_type", label = "Variable 2 Type", choices = c("Factor","Numerical"))
-  })
-  
-  observeEvent(input$run_button,{
-    
-    var_1 <- renderText({input$var_1})
-    var_1_type <- renderText({input$var_1_type})
-    var_2 <- renderText({input$var_2})
-    var_2_type <- renderText({input$var_2_type})
-    
-      
-    if(var_1_type() == "Numerical" & var_2_type() == "Numerical"){
-      plot_1 <- ggplot(data = data_input(), aes_string(x = var_1(), y = var_2())) + geom_point()
-    }
-    else if(var_1_type() == "Numerical" & var_2_type() == "Factor"){
-      plot_1 <- ggplot(data = data_input(), aes_string(x = var_1(), color = var_2())) + geom_density()
-    }
-    
-    if(input$log_x == TRUE){plot_1 <- plot_1 + scale_x_log10()}
-    if(input$log_y == TRUE){plot_1 <- plot_1 + scale_y_log10()}
-
-  })
-  
-  output$plot_1 <- renderPlot({plot_1})
-  
-  
 }
 
 shinyApp(ui = ui, server = server)
